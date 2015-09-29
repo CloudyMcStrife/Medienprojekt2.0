685d16092d0fe6767a852212dd7ddafe74ea6833
Skeletti <schreiber.daniel87@gmail.com>
2015-09-29 18:18:12 +0200

    Healthbar Handler Implemented (includes Colorchange)

---
+++ Medienprojekt2.0/Assets/Assets/Scripts/UI/HealthbarHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthbarHandler : MonoBehaviour {
+
+    RectTransform healthTransform;
+    float cachedY;
+    float maxXPos;
+    float minXPos;
+    float currentHealth;
+    float maxHealth;
+    float newXPos;
+    public AttributeComponent attComp;
+    Image visualHealth;
+
+    float oldXValue;
+    bool healthIsTriggered;
+    float timer = 1;
+
+	// Use this for initialization
+	void Start () {
+        healthTransform = this.GetComponent<RectTransform>();
+        cachedY = 0;
+        Debug.Log("YOYO " + cachedY);
+        maxXPos = healthTransform.localPosition.x;
+        minXPos = healthTransform.localPosition.x - healthTransform.rect.width;
+        maxHealth = attComp.getHealth();
+        currentHealth = maxHealth;
+        oldXValue = 0;
+        healthIsTriggered = false;
+        visualHealth = this.GetComponent<Image>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        oldXValue = newXPos;
+        currentHealth = attComp.getHealth();
+        newXPos = MapValues(currentHealth, 0, maxHealth, minXPos, maxXPos);
+
+        if (oldXValue != newXPos)
+        {
+            timer -= Time.deltaTime;
+            healthTransform.localPosition = new Vector3(newXPos + (oldXValue - newXPos)*timer, cachedY);
+            if (timer < 0)
+            {
+                oldXValue = newXPos;
+            }
+        }
+
+        if(currentHealth > maxHealth/2)
+        {
+            visualHealth.color = new Color32((byte)MapValues(currentHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
+        }
+
+        else
+        {
+            visualHealth.color = new Color32(255, (byte)MapValues(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
+        }
+
+
+    }
+
+    private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}

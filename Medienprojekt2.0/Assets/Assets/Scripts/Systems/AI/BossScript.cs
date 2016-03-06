b72d3063918efa25c1c7f268e5222dec395800f1
Shentoza <wuelk@hotmail.de>
2016-03-06 21:26:09 +0100

    boss anim + start of script and  stuff.... todo: fighting logic,hitzones of hands

---
+++ Medienprojekt2.0/Assets/Assets/Scripts/Systems/AI/BossScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossScript : MonoBehaviour
+{
+    Animator anim;
+    bool spawned = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = (Animator)GetComponent(typeof(Animator));
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //who won? who's next? You decide! Epic Battles of history :D
+        FightingDecisions();
+    }
+
+
+    //Called by Spawn Trigger to spawn the boss
+    public void Spawn()
+    {
+        spawned = true;
+        anim.SetTrigger("Spawn");
+    }
+
+    void FightingDecisions()
+    {
+
+    }
+}

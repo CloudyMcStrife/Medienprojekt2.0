e29d1c512a355f03a5f8049626b4ab4a97b11a54
gimmillf@hochschule-trier.de <gimmillf@hochschule-trier.de>
2016-03-11 17:13:49 +0100

    Lichter werden angeschaltet wenn SPieler Ebene betritt

---
+++ Medienprojekt2.0/Assets/Assets/Scripts/FloorLightTrigger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorLightTrigger : MonoBehaviour {
+
+    GameObject floorLights;
+    float timer;
+    bool lightsEnabled;
+
+	// Use this for initialization
+	void Start () {
+        Transform floorTransform = transform.Find("FloorLights");
+        floorLights = floorTransform.gameObject;
+        timer = 0.0f;
+        lightsEnabled = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lightsEnabled)
+        {
+            if(timer < 2.0f)
+            {
+                int deg = (int)(Mathf.Rad2Deg * timer);
+                if ((deg % 2) == 0)
+                    floorLights.SetActive(true);
+                else
+                    floorLights.SetActive(false);
+            }
+            else
+            {
+                floorLights.SetActive(true);
+            }
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            floorLights.SetActive(false);
+            timer = 0;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+            lightsEnabled = true;
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.name == "Player")
+            lightsEnabled = false;
+    }
+}

49acd4768241493ecb78278fc4c0fa8cb961ace8
gimmillf@hochschule-trier.de <gimmillf@hochschule-trier.de>
2016-03-13 14:43:30 +0100

    Thunder simulation works

--- Medienprojekt2.0/Assets/ThunderComponent.cs
+++ Medienprojekt2.0/Assets/ThunderComponent.cs
@@ -4,12 +4,30 @@ using System.Collections;
 public class ThunderComponent : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public float offDurationMin;
+    public float offDurationMax;
+
+    public float onDurationMin;
+    public float onDurationMax;
+
+    Light l;
+
+    void Start()
+    {
+        l = GetComponent<Light>();
+        StartCoroutine(DoOnOff());
+    }
+
+    IEnumerator DoOnOff()
+    {
+        while (true)
+        {
+            l.enabled = false;
+            yield return new WaitForSeconds(Random.Range(offDurationMin, offDurationMax));
+            l.enabled = true;
+            yield return new WaitForSeconds(Random.Range(onDurationMin, onDurationMax));
+        }
+    }
+
 }
